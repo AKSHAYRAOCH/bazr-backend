078eb66e0686f9492b810f456da040acae97d379
AKSHAYRAOCH <chakshayrao@gmail.com>
2025-06-05 18:54:43 +0530

    feat: implement basic Express server with user CRUD endpoints and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { config } from "dotenv";
+import db from "./db/db";
+import { usersTable } from "./db/schema";
 
 
 config()
 const app = express()
+app.use(express.json())
 
-app.get("/", async (req, res) => {
-    res.send("Hello World")
+app.get("/getusers", async (req, res) => {
+    const users = await db.select().from(usersTable)
+    res.json({
+        users
+    })
     
     
+});
+
+app.post("/adduser",async (req,res)=>{
+    const data = req.body;
+    const inserted = await db.insert(usersTable).values({name:data.name,age:data.age,email: data.email}).returning()
+    
+    res.json({
+        message:"User added successfully"
+    })
+    
+})
+
+
+
+// Error handling middleware - MUST be defined after all other app.use() and routes
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack); // Log the error stack for debugging - IMPORTANT for server-side
+
+  // Send a generic error response
+  res.status(500).json({
+    message: 'Something went wrong'
+  });
 });
 
 app.listen(3000, () => {
