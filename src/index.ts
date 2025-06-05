b8088df5c937d64149b408c0cbdd09f409fe1cc1
AKSHAYRAOCH <chakshayrao@gmail.com>
2025-06-05 17:55:20 +0530

    init: create Express.js backend project with TypeScript configuration and basic server setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { config } from "dotenv";
+config()
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+    console.log(process.env.POSTGRES_URL)
+})
